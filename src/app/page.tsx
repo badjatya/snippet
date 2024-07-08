47332dc44ac183df06c00ac64db5c2bdc4bef2f1
badjatya <architj240@gmail.com>
2024-07-08 18:36:24 +0530

    feat: Add Hero component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Navbar from "@/layouts/Navbar";
+import Hero from "./components/Hero";
 
 export default function Home() {
-	return <Navbar />;
+	return (
+		<div className='bg-white pb-6 sm:pb-8 lg:pb-12'>
+			<div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
+				<Navbar />
+				<Hero />
+			</div>
+		</div>
+	);
 }
