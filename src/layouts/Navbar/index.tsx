d672aaa72ce49fa8ef97dda1fc4552b22fe7c52c
badjatya <architj240@gmail.com>
2024-07-09 00:35:58 +0530

    feat: Add Dashboard page component

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -10,43 +10,47 @@ const Navbar = (props: Props) => {
 	const { user, isSignedIn } = useUser();
 	console.log("User: ", user);
 	return (
-		<header className='mb-8 flex items-center justify-between py-4 md:mb-12 md:py-8 xl:mb-16'>
-			<Link
-				href='/'
-				className='inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl'
-				aria-label='logo'>
-				<Image
-					src='/logo/logo.png'
-					alt='Logo'
-					width={165}
-					height={63}
-					quality={100}
-				/>
-			</Link>
+		<div className='bg-white'>
+			<div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
+				<header className='mb-8 flex items-center justify-between py-4 md:mb-12 md:py-8 xl:mb-16'>
+					<Link
+						href='/'
+						className='inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl'
+						aria-label='logo'>
+						<Image
+							src='/logo/logo.png'
+							alt='Logo'
+							width={165}
+							height={63}
+							quality={100}
+						/>
+					</Link>
 
-			<nav className='hidden gap-12 lg:flex'>
-				<Link
-					href='/about'
-					className='text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary'>
-					About
-				</Link>
-				<Link
-					href='/contact'
-					className='text-lg text-gray-600 transition duration-100 font-semibold hover:text-primary'>
-					Contact
-				</Link>
-			</nav>
+					<nav className='hidden gap-12 lg:flex'>
+						<Link
+							href='/about'
+							className='text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary'>
+							About
+						</Link>
+						<Link
+							href='/contact'
+							className='text-lg text-gray-600 transition duration-100 font-semibold hover:text-primary'>
+							Contact
+						</Link>
+					</nav>
 
-			{isSignedIn ? (
-				<Button asChild>
-					<Link href='/dashboard'>Dashboard</Link>
-				</Button>
-			) : (
-				<Button asChild>
-					<Link href='/sign-up'>Signup</Link>
-				</Button>
-			)}
-		</header>
+					{isSignedIn ? (
+						<Button asChild>
+							<Link href='/dashboard'>Dashboard</Link>
+						</Button>
+					) : (
+						<Button asChild>
+							<Link href='/sign-up'>Signup</Link>
+						</Button>
+					)}
+				</header>
+			</div>
+		</div>
 	);
 };
 
---
+++ src/layouts/Sidebar/index.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { Home, User, Settings, Calendar, PhoneCall } from "lucide-react";
+
+const SidebarData = [
+	{
+		name: "Base",
+		items: [
+			{
+				name: "Home",
+				icon: Home,
+				path: "/dashboard/mentee",
+			},
+			{
+				name: "Profile",
+				icon: User,
+				path: "/dashboard/mentee/profile",
+			},
+			{
+				name: "Settings",
+				icon: Settings,
+				path: "/dashboard/mentee/settings",
+			},
+		],
+	},
+	{
+		name: "Meetings",
+		items: [
+			{
+				name: "Calendar",
+				icon: Calendar,
+				path: "/dashboard/mentee/calendar",
+			},
+			{
+				name: "Bookings",
+				icon: PhoneCall,
+				path: "/dashboard/mentee/bookings",
+			},
+		],
+	},
+];
+
+const Sidebar = () => {
+	return (
+		<div className='flex flex-col h-screen p-4 lg:p-6 relative'>
+			{/* Sidebar */}
+			<div
+				className={`fixed inset-y-0 left-0 transform md:relative w-48 `}>
+				{/* Logo Section */}
+				<Link href='/'>
+					<div className='relative w-32 h-8 md:w-40 md:h-8'>
+						<Image src='/logo/logo.png' alt='snippet logo' fill />
+					</div>
+				</Link>
+
+				{/* Navigation */}
+				<div className='flex-grow mt-8'>
+					<nav className='flex flex-col space-y-4'>
+						{SidebarData.map((item) => (
+							<div key={item.name} className='space-y-1'>
+								<div className='text-sm font-semibold text-gray-500'>
+									{item.name}
+								</div>
+								<div className='flex flex-col space-y-1'>
+									{item.items.map((subItem) => (
+										<Link
+											key={subItem.name}
+											href={subItem.path}
+											className='flex items-center space-x-2 p-2 rounded-md hover:bg-blue-200 transition-colors ease-in duration-100'>
+											<subItem.icon className='w-5 h-5' />
+											<span>{subItem.name}</span>
+										</Link>
+									))}
+								</div>
+							</div>
+						))}
+					</nav>
+				</div>
+			</div>
+			{/* Logout Button */}
+			<div className='hidden lg:block absolute bottom-0 w-[79%] h-16'>
+				<button className='flex w-full items-center space-x-2 p-2 rounded-md hover:bg-blue-200 transition-colors ease-in duration-100'>
+					<LogOut className='w-5 h-5' />
+					<span>Logout</span>
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
