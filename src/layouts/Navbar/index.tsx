7a3705d7c29ea53850393ec14769c2d4889b7dca
badjatya <architj240@gmail.com>
2024-07-08 16:15:38 +0530

    feat: base layout

---
+++ src/layouts/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+	return (
+		<>
+			<div>Navbar</div>
+			<Button>Click me</Button>
+		</>
+	);
+};
+
+export default Navbar;
